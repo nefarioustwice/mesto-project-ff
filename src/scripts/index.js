e416046fa636028f8ee4128ce917037f825d0986
Vitaliy Mankein <NefariousTwice@gmail.com>
2024-08-24 16:07:52 +0700

    Перенос проекта на webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,20 @@
+import '../pages/index.css';
+import {initialCards} from './cards.js';
+
+import avatar from '../images/avatar.jpg';
+import card1 from '../images/card_1.jpg';
+import card2 from '../images/card_2.jpg';
+import card3 from '../images/card_3.jpg';
+import logo from '../images/logo.svg';
+
+const whoIsTheGoat = [
+    { name: 'avatar', link: avatar },
+    { name: 'card1', link: card1 },
+    { name: 'card2', link: card2 },
+    { name: 'card3', link: card3 },
+    { name: 'logo', link: logo },
+]; 
+
 const cardTemplate = document.querySelector('#card-template');
 const placesList = document.querySelector('.places__list');
 
